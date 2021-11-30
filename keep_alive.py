c22602cff5a8637b37858630112f5529f0c5b740
Flamingo <aarooni2018@gmail.com>
2021-11-30 10:21:28 +0530

    Update keep_alive.py

--- keep_alive.py
+++ keep_alive.py
@@ -1,14 +1,15 @@
+#creating a web server for our bot and continuously ping this web server using uptime Robot 
 from flask import Flask
-from threading import Thread
+from threading import Thread  #the server will run on a separate thread from our bot
 
 
-app = Flask('')
+app = Flask('') #creating a flask app
 
 @app.route('/')
 def home():
-  return "Music Math discord py bot is alive >.<"
+  return "Music Math discord py bot is alive >.<" #displays this to anyone who visits the server . The URL is created by repl.it
 
-def run():
+def run(): #runs the web server
   app.run(host="0.0.0.0",port = 8080)
 
 def keep_alive():
